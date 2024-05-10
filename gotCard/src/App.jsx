51dc14ba8fc4711b3481539169938066bba016e6
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-05-10 18:47:52 +0200

    maj searchbar, using children components props toward parents components

--- gotCard/src/App.jsx
+++ gotCard/src/App.jsx
@@ -1,13 +1,32 @@
 import "./style/App.css";
 import Card from "./components/Card";
 import NavBar from "./components/NavBar";
+import SearchBar from "./components/SearchBar";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [filteredCharacters, setFilteredCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://thronesapi.com/api/v2/Characters")
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data);
+        setFilteredCharacters(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <>
+    <section>
       <NavBar />
-      <Card />
-    </>
+      <SearchBar
+        characters={characters}
+        setFilteredCharacters={setFilteredCharacters}
+      />
+      <Card characters={filteredCharacters} />
+    </section>
   );
 }
 
